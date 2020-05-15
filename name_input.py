4cd243330fe61ca92225c785271d15d4dd3fa883
Lyn Stanford <lynstanford@users.noreply.github.com>
2020-05-15 12:43:56 -0400

    Delete name_input.py

--- name_input.py
+++
@@ -1,8 +0,0 @@
-name = input('Please enter your name :')        # the variable stored in this input() function can be called using print()
-print(name)
-
-# Examples of using blank lines to improve readability
-print('Hi there')
-print()             # prints a blank line
-print('Did you see that blank line?')
-print('Blank line \nin the middle of a string')
