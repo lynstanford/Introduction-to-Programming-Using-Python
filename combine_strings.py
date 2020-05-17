7b359d75d83f46014697f6b632c2b3d5151e3c16
Lyn Stanford <lynstanford@outlook.com>
2020-05-17 17:40:49 -0400

    String Concatenation
    Combining 'string literals' and' variables'

---
+++ combine_strings.py
@@ -0,0 +1,4 @@
+first_name = 'Christopher'                    # You can use single or double quotes
+last_name = 'Harrison'
+print(first_name + last_name)
+print('Hello ' + first_name + ' ' + last_name)
---
+++ string_variables.py
@@ -0,0 +1,2 @@
+first_name = 'Christopher'
+print(first_name)
