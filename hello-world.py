1901d14e0e01475a64a4fa788db181970f88e3b1
Lyn Stanford <lynstanford@outlook.com>
2020-05-17 14:23:13 -0400

    Changes to Print()
    Hello World file, printing on different lines, escape character and debugging using print statements

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,8 @@
 print("hello world")
+
+# also, you can print output on separate lines as follows
+print("Hello")
+print("World")
+
+# or using the escape character \n (to start a new line)
+print("Hello Lyn, \nWhat a wonderful day!")
---
+++ print_debugging_2.py
@@ -0,0 +1,5 @@
+print('Adding numbers')
+x = 532 + 12
+print('Dividing numbers')
+y = x /0
+print('yay I did math')
