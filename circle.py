08f186437c600f953461cc2c8420fed95cf47302
Lyn Stanford <lynstanford@outlook.com>
2020-05-17 13:37:38 -0400

    Initial files uploaded
    Python tutorial files so far

---
+++ circle.py
@@ -0,0 +1,12 @@
+from turtle import *
+import turtle as turtle
+
+pensize(30)
+turtle.left(90)
+turtle.circle(100)
+turtle.color('red')
+turtle.bgcolor('orange')
+turtle.degrees(350)
+turtle.left(90)
+turtle.forward(50)
+
