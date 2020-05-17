f0b108892279192582099cbbab5ba2bdbb6db3bb
Lyn Stanford <lynstanford@outlook.com>
2020-05-17 17:05:19 -0400

    Create comment_debugging.py
    Using comments to debug code, single and double quotes for strings

---
+++ comment_debugging.py
@@ -0,0 +1,9 @@
+# the next line of code uses single quotes and throws an error
+print('This line of code's wrong')
+
+# but we can test by commenting out the line of code in question and changing it to include double quotes for the string
+# print('This line of code's wrong') - this is an example of debugging using comments
+print("This line of code's correct")
+
+# also use CTRL-K-C to turn the current selected code line into a comment
+# CTRL-K-U to un-comment
