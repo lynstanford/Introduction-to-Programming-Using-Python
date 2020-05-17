acfa16c3009b0ec1ceb7323ab1926e95107a95b7
Lyn Stanford <lynstanford@outlook.com>
2020-05-17 18:34:26 -0400

    Create input_concat.py
    String concatenation using input function

---
+++ input_concat.py
@@ -0,0 +1,7 @@
+'''The functions help us format strings we save
+to files and databases, or display to users'''
+
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+print('Hello ' + first_name.capitalize() + ' ' \
+  + last_name.capitalize())
